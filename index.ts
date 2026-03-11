456281a57186d0f99c8aa9bce467538c9fd5c988
carlosbadillar <A01255765@tec.mx>
2026-03-10 17:04:35 -0700

    feat: crud completo de productos y conexion a bd

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import morgan from 'morgan';
-import apiRouter from './src/routes'; // Importamos el grupo de rutas
+import productRouter from './src/routes/productRoutes';
+import connectionDB from './src/connection/connection';
 
 const app: Express = express();
 const port: number = 3000;
 
 app.use(morgan('dev'));
-app.use(express.json()); // ¡Súper importante para que entienda los datos de Postman!
-app.use(apiRouter); // Usamos el router central
+app.use(express.json());
+
+connectionDB();
+
+app.use('/product', productRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
